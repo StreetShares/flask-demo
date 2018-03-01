9d72523dabfa0d760a61618b863ae0ebee5c7bc9
Andrew Ahlers <aahlers@streetshares.com>
2018-03-01 13:03:14 -0500

    Minor updates while I set things up.

--- dev_run.py
+++ dev_run.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 
-"""Hacky way to start all servers at once."""
+"""Hacky way to start/stop all servers at once."""
 
 import os
 import subprocess
+
+
+def kill_processes(processes):
+    """Kill all process in a list of processes."""
+    print(processes)
+    if not processes:
+        return
+    for i, proc in enumerate(processes):
+        try:
+            proc.terminate()
+            processes.pop(i)
+        except OSError:
+            pass
+    print('All servers have been murdered.')
 
 
 dirpath = os.path.dirname(os.path.realpath(__file__))
@@ -22,11 +36,14 @@ for server in SERVERS:
     processes.append(subprocess.Popen(['python', server_path]))
 
 try:
+    # for proc in processes:
+    #     try:
+    #         proc[0].wait()
+    #     except Exception:
+    #         pass
     processes[0].wait()
 except KeyboardInterrupt:
-    for proc in processes:
-        try:
-            proc.terminate()
-        except OSError:
-            pass
-    print('All servers have been murdered.')
+    kill_processes(processes)
+
+# Incase we get down here but didn't kill everything.
+kill_processes(processes)
