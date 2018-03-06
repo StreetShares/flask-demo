3e3bf4b942b7610917acbcaef21ffaac97e20a13
Andrew Ahlers <aahlers@streetshares.com>
2018-03-06 09:21:24 -0500

    Fixing typo in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='syncmachine_flask',
+    name='flask_demo',
     version='1',
     packages=find_packages(exclude=['tests']),
 )
